e8cefb1fa78a78eaadebaaf1aaa3db1283a67148
Lucas Oliveira Santana da Cunha <lukkzmaverick@gmail.com>
2020-09-23 10:39:43 -0300

    Adicionada Variáveis de Ambiente

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const connectDB = require('./config/db.js')
-const port = 3001
+const port = process.env.PORT
 
 app.use(cors())
 
@@ -13,8 +14,20 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use('/api/jogos', require('./routes/api/jogo'))
 app.use('/api/user', require('./routes/api/user'))
 
+app.use (function (req, res, next) {
+    var schema = (req.headers['x-forwarded-proto'] || '').toLowerCase();
+    if (req.headers.host.indexOf('localhost') < 0 && schema !== 'https') {
+        res.redirect('https://' + req.headers.host + req.url);
+    }
+    next();
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname))
+})
+
 connectDB()
 
-app.listen(port, () => console.log(`Localhost at ${port}`))
+app.listen(port, () => console.log(`Port: ${port}`))
 
 
--- middleaware/auth.js
+++ middleaware/auth.js
@@ -1,12 +1,11 @@
 const jwt = require('jsonwebtoken')
-const config = require('config')
 
 module.exports = function (request, response, next) {
     const token = request.header('x-auth-token')
     if(!token){
         return response.status(401).json({errors: [{msg: "Token faltando, autorização negada!"}]})
     }
-    jwt.verify(token, config.get('jwtSecret'), (error, decoded) => {
+    jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
         if(error){
             return response.status(401).json({errors: [{msg: "Token inválido, autorização negada!"}]})
         }
